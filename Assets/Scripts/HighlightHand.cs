904e9cbde08f974f4f5f2a7ac472ddf00f62866f
nakisa <nakisa.donnelly@gmail.com>
2021-09-20 13:31:06 -0700

    Added Unity project

---
+++ Assets/Scripts/HighlightHand.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighlightHand : MonoBehaviour
+{
+    public Material highlightedMaterial;
+    public Material unhighlightedMaterial;
+    public GameObject highlightObject;
+       
+    void Start()
+    {
+        
+    }
+
+    public void Highlight()
+    {
+        Debug.Log("highlight");
+        highlightObject.GetComponent<Renderer>().material = highlightedMaterial;
+    }
+
+    public void UnHighlight()
+    {
+        Debug.Log("unhighlight");
+        highlightObject.GetComponent<Renderer>().material = unhighlightedMaterial;
+    }
+   
+}
